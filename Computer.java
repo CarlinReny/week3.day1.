aa8720ba9025c21080b868db99131cfb80f3881e
CarlinReny <96654645+CarlinReny@users.noreply.github.com>
2022-01-19 11:30:08 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,20 @@
+package org.system;
+
+public class Computer {
+	
+	
+	public static void computerModel() {
+		System.out.println("This is the Computer Model");
+		
+	}
+
+	
+	public static void main(String[] args) {
+		
+		
+		computerModel();
+		
+	}
+
+	
+}
---
+++ Desktop.java
@@ -0,0 +1,22 @@
+package org.system;
+
+public class Desktop extends Computer{
+	
+	
+	private static void desktopsize() {
+		System.out.println("This is the desktop size");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		desktopsize();
+		computerModel();
+		
+	}
+
+	
+	
+
+}
